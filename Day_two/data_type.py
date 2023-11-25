f30b4396f45024a9b69093655851f16370589ffa
Maxwell <mboakye@excenit.com>
2023-11-25 23:58:59 +0000

    completed day two project

---
+++ Day_two/data_type.py
@@ -0,0 +1,13 @@
+# data type
+print("Hello" [-1])
+
+# commas in number can be replace by underscore
+print(123_123_245)
+
+print(3 * (3 + 3 )/ 3 - 3)
+
+score = 0
+height = 1.6
+isWinning = True
+
+print(f'your is score is {score}, your height is {height}, you are winning is {isWinning}')
---
+++ Day_two/tip_calculator.py
@@ -0,0 +1,8 @@
+print('Welcome to the tip calculator.')
+total_bill = float(input('What is the total bill? $'))
+tip_percentage = float(input('What percentge tip would you like to give? 10, 12, or 15? '))
+bill_split = float(input('How many people to split the bill? '))
+
+tip_and_bill = total_bill * (tip_percentage /100) 
+bill_payment = (tip_and_bill + total_bill)/ bill_split
+print(f'Each person should pay: ${round(bill_payment,2)}')
